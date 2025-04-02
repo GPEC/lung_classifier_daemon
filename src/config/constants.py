46a8334b0df0b970209590cc7e55b082ddbd4ff2
Samuel Leung <sam.leung@gmail.com>
2025-04-02 00:47:50 -0700

    updated comments

--- src/config/constants.py
+++ src/config/constants.py
@@ -5,6 +5,10 @@ import os, pandas as pd
 WORKING_FOLDER = "C:\\Users\\samleung\\Documents\\workspace-py\\lung_classifier_daemon\\src"
 DATA_FOLDER = "C:\\Users\\samleung\\Documents\\workspace-py\\lung_classifier_daemon_data"
 
+# python venv - executable; this will be used in os.system() to run the classifier code
+# - SERVER-SPECIFIC / NEED UPDATE
+PYTHON_EXE = "C:\\Users\\samleung\\AppData\\Local\\Microsoft\\WindowsApps\\python3.exe"
+
 # read REDCap tokens
 RC_TOKEN_FNNAME = os.path.join(DATA_FOLDER,"REDCAP_TOKENS.csv")
 rc_token_d = pd.read_csv(RC_TOKEN_FNNAME)
@@ -26,6 +30,3 @@ input_template_fname = "data\\input_beta_example.txt"
 OUTPUT_DIR = os.path.join(DATA_FOLDER,"user_data")
 # name of SQLite database file (for storing info such as requestor names, emails)
 SQLITE_FNAME = os.path.join(OUTPUT_DIR,"lcd.sqlite")
-
-# python venv - executable; this will be used in os.system() to run the classifier code
-PYTHON_EXE = "C:\\Users\\samleung\\AppData\\Local\\Microsoft\\WindowsApps\\python3.exe"
